da22069e9d183de1496bfaa799de43217328ead3
Ujjwal Sharma <ujjwalsharma@protonmail.com>
2017-10-19 22:28:23 +0200

    Added Range and Bearing Measurements

--- main.py
+++ main.py
@@ -4,7 +4,6 @@
 import os
 import time
 
-import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -42,14 +41,118 @@ def remove_non_informative_regions(img):
     return croppedImage
 
 
+def show_data(range_measurement, bearing_measurement):
+    # Displays Range and Measurment Values
+
+    print("Current Range from Landmark Detected is %0.2f meters" %
+          range_measurement)
+    print("Current Global Angle is %0.2f degrees" % bearing_measurement)
+
+    return (0)
+
+# Range and Estimation Calculations by Ward Heij.
+# estimate distance from a landmark given the size of a detection
+
+
+def estimate_range(landmark_location):
+
+    # determine width and hieght of the patch
+    #width = bottom_right[0] - top_left[0]
+    #height = bottom_right[1] - top_left[1]
+
+    # Unpack landmark location values
+    (_, _, width, height, _) = landmark_location
+
+    # set nominal values at 1 meter distance TODO: Check if this works correctly
+    w_nominal = 47.16
+    h_nominal = 107.32
+
+    # estimate distance based on width/height ratios independently and average
+    d1 = w_nominal / width
+    d2 = h_nominal / height
+    range_estimate = (d1 + d2) / 2.0
+
+    return range_estimate
+
+
+def get_landmark_angle_radians(signature):
+    if signature == 1:
+        angle = 5.4979
+    if signature == 2:
+        angle = 0
+    if signature == 3:
+        angle = 0.785398
+    if signature == 4:
+        angle = 2.35619
+    if signature == 5:
+        angle = 3.14159
+    if signature == 6:
+        angle = 3.92699
+
+    return angle
+
+
+def get_landmark_angle_degrees(signature):
+    if signature == 1:
+        angle = 315
+    if signature == 2:
+        angle = 0
+    if signature == 3:
+        angle = 45
+    if signature == 4:
+        angle = 135
+    if signature == 5:
+        angle = 180
+    if signature == 6:
+        angle = 225
+
+    return angle
+
+# estimate the bearing of the robot with respect to the landmark
+
+
+def estimate_bearing(img, landmark_patch, range_measurement):
+
+    fov = 60.9  # degrees, from Nao documentation
+
+    hI, wI, channels = img.shape
+    xcenterI = int(wI / 2)
+    ycenterI = int(hI / 2)
+
+    # Unpack landmark location values
+    (xL, yL, wL, hL, signature) = landmark_location
+
+    # find center of the observation, find distance from image center
+    xcenterL = (xL + (xL + wL)) / 2.0
+    ycenterL = (yL + (yL + hL)) / 2.0
+
+    diff_angle = np.arctan(abs(xcenterI - xcenterL) / range_measurement)
+
+    d = abs(xcenterI - xcenterL)
+
+    # compute bearing from pixel difference, corrected for head pitch
+    diff_angle = (float(d) / float(w)) * fov
+    landmark_angle = get_landmark_angle_degrees(signature)
+
+    if (xcenterI - xcenterL >= 0):
+        global_heading_angle = landmark_angle - diff_angle
+
+    if (xcenterI - xcenterL < 0):
+        global_heading_angle = landmark_angle + diff_angle
+
+    #bearing_measurement = np.rad2deg(global_heading_angle)
+    bearing_measurement = global_heading_angle
+    return bearing_measurement
+
+
 startTime = time.time()
 detections = []
-fileList = sorted(os.listdir('calibration'))
+fileList = sorted(os.listdir('test'))
 
-for idx, file in enumerate(fileList):
+for file in fileList:
 
     # This loop loads image to be tested.
-    img = cv2.imread('calibration/' + file)
+    img = cv2.imread('test/' + file)
     img = remove_non_informative_regions(img)
     img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     #img = histogramEqualize(img)
@@ -84,20 +187,36 @@ for idx, file in enumerate(fileList):
     for loc in pinkLocations:
 
         (x, y, w, h) = loc
+
+        # Orientation parameter '-1' suugests that the detected pink patch is on
+        # the bottom of the landmark. Conversely '+1' suggests pink patch is at
+        # the top.
+
         patches = [tuple([x, y - h, w, h, -1]), tuple([x, y + h, w, h, 1])]
 
         for patch in patches:
+
+            landmark_location = None
             (x, y, w, h, orientation) = patch
             out = img_hsv[y:y + h, x:x + w]
             avg_hsv = np.average(np.average(out, axis=0), axis=0)
+
             if (LOWER_GREEN[0] <= avg_hsv[0] <= UPPER_GREEN[0] and
                 LOWER_GREEN[1] <= avg_hsv[1] <= UPPER_GREEN[1] and
                     LOWER_GREEN[2] <= avg_hsv[2] <= UPPER_GREEN[2]):
 
                 if (orientation == 1):
                     detections.append(tuple([file, "Pink_Green"]))
+                    landmark_location = tuple([x, y - h, w, 2 * h, 5])
                 else:
                     detections.append(tuple([file, "Green_Pink"]))
+                    landmark_location = tuple([x, y, w, 2 * h, 2])
+
+                range_measurement = estimate_range(landmark_location)
+                bearing_measurement = estimate_bearing(img, landmark_location,
+                                                       range_measurement)
+
+                show_data(range_measurement, bearing_measurement)
 
             if (LOWER_BLUE[0] <= avg_hsv[0] <= UPPER_BLUE[0] and
                 LOWER_BLUE[1] <= avg_hsv[1] <= UPPER_BLUE[1] and
@@ -105,8 +224,16 @@ for idx, file in enumerate(fileList):
 
                 if (orientation == 1):
                     detections.append(tuple([file, "Pink_Blue"]))
+                    landmark_location = tuple([x, y - h, w, 2 * h, 6])
                 else:
                     detections.append(tuple([file, "Blue_Pink"]))
+                    landmark_location = tuple([x, y, w, 2 * h, 1])
+
+                range_measurement = estimate_range(landmark_location)
+                bearing_measurement = estimate_bearing(img, landmark_location,
+                                                       range_measurement)
+
+                show_data(range_measurement, bearing_measurement)
 
             if (LOWER_YELLOW[0] <= avg_hsv[0] <= UPPER_YELLOW[0] and
                 LOWER_YELLOW[1] <= avg_hsv[1] <= UPPER_YELLOW[1] and
@@ -114,8 +241,16 @@ for idx, file in enumerate(fileList):
 
                 if (orientation == 1):
                     detections.append(tuple([file, "Pink_Yellow"]))
+                    landmark_location = tuple([x, y - h, w, 2 * h, 4])
                 else:
                     detections.append(tuple([file, "Yellow_Pink"]))
+                    landmark_location = tuple([x, y, w, 2 * h, 3])
+
+                range_measurement = estimate_range(landmark_location)
+                bearing_measurement = estimate_bearing(img, landmark_location,
+                                                       range_measurement)
+
+                show_data(range_measurement, bearing_measurement)
 
 
 print("--This run took %0.2f seconds--" % (time.time() - startTime))
